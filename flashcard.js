a4e3f0a43d99bc4f05a70e9266e118cfa456dec0
ESAM SHARAF <asoomsharaf@hotmail.com>
2018-06-01 11:37:35 +0200

    first-commit

---
+++ flashcard.js
@@ -0,0 +1,4 @@
+var wordPairs = [["fiets","bike"],["auto","car"],["lift","elevator"],["deur","door"],["tafel","table"]
+,["stoel","chair"],["beker","mug"],["muur","wall"],["zon","sun"],["regen","rain"]]
+
+document.write((wordPairs[1])[0]);
